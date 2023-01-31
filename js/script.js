bf89acb56c950babdbb4f36c816f31fb41ed0e26
raulblancoq <rwhite8289@gmail.com>
2023-01-31 10:32:56 -0500

    error msg

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('form');
 const thankyou = document.getElementById('thankYou');
 const rateValue = document.getElementById('rateValue');
+const error = document.getElementById('error');
 
 form.addEventListener('submit',Rate)
 
@@ -11,11 +12,11 @@ function Rate(e) {
     for (let i = 0; i < collection.length; i++) {
         if (collection[i].checked) {
             form.classList.add('hidden');
-            
+            thankyou.classList.remove('hidden');
             rateValue.innerHTML = collection[i].value;
         } else {
-            console.log('falta seleccionar')
+            error.innerHTML = 'Choose an option';
         }
     }
-    thankyou.classList.remove('hidden');
+    
 }
